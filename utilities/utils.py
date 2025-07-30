348556f6fdf507fe2dcc676fffd8b20d85746b9e
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-07-31 03:52:42 +0530

    utils: remove print statements
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,17 +1,18 @@
 import requests
 import os
 
+import streamlit as st
+
 
 def fetch_image(movie_id: int) -> str:
     url = f"https://api.themoviedb.org/3/movie/{movie_id}"
     headers = {
-        "Authorization": f"Bearer {os.getenv('TMDB_API_KEY')}",
+        "Authorization": f"Bearer {st.secrets.get('TMDB_API_KEY')}",
         "accept": "application/json"
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     data = response.json()
-    print(data)
 
     poster_path = data.get("poster_path")
 
