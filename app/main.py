adbfa2b1a83bd8848291af86154ff359b65b8864
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-07-31 11:49:21 +0530

    ui: improve the search field

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import os
 sys.path.insert(0, os.path.curdir)
 
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 from model.recommender import load_processed, compute_similarity, recommend
 from utilities.utils import fetch_image
 
@@ -10,14 +11,18 @@ df = load_processed()
 similarity = compute_similarity(df)
 
 st.title("🎥Cine Suggest")
-movie_name = st.selectbox("Select a movie:", df["title"].values)
+search = st.text_input("Type to search a movie")
+filtered = [title for title in df["title"] if search.lower() in title.lower()]
+
+if search:
+    movie_name = st.selectbox("Pick from results", filtered if filtered else ["No match"])
 
 
-if st.button("Recommend"):
-    with st.spinner("Generating recommendations..."):
-        recommendations = recommend(movie_name, df, similarity)
-        cols = st.columns(len(recommendations))
-        for col, (name, movie_id) in zip(cols, recommendations.items()):
-            with col:
-                st.image(fetch_image(movie_id))
-                st.text(name)
+    if st.button("Recommend"):
+        with st.spinner("Generating recommendations..."):
+            recommendations: dict = recommend(movie_name, df, similarity)
+            cols: list[DeltaGenerator] = st.columns(len(recommendations))
+            for col, (name, movie_id) in zip(cols, recommendations.items()):
+                with col:
+                    st.image(fetch_image(movie_id))
+                    st.text(name)
