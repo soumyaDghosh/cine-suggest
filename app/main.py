3b6885ab7bec0712c260824119c536cd6c4c024e
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-08-02 18:23:36 +0530

    ui: improve the search
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

--- app/main.py
+++ app/main.py
@@ -12,17 +12,22 @@ similarity = compute_similarity(df)
 
 st.title("🎥Cine Suggest")
 search = st.text_input("Type to search a movie")
-filtered = [title for title in df["title"] if search.lower() in title.lower()]
+filtered = [title for title in df["title"] if search.replace(" ", "").lower() in title.replace(" ", "").lower()]
 
 if search:
     movie_name = st.selectbox("Pick from results", filtered if filtered else ["No match"])
 
 
-    if st.button("Recommend"):
+    if filtered and st.button("Recommend"):
         with st.spinner("Generating recommendations..."):
             recommendations: dict = recommend(movie_name, df, similarity)
             cols: list[DeltaGenerator] = st.columns(len(recommendations))
-            for col, (name, movie_id) in zip(cols, recommendations.items()):
+            for col, (i, (name, movie_id)) in zip(cols, enumerate(recommendations.items())):
+                try:
+                    image = fetch_image(movie_id=movie_id)
+                except:
+                    image = "https://via.placeholder.com/500x750?text=No+Image"
                 with col:
-                    st.image(fetch_image(movie_id))
+                    st.header(i+1)
+                    st.image(image)
                     st.text(name)
